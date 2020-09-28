0ad8b8d2a5d86277ab97eed5dcde7e44e0e1c378
Nataliya <an.chishevich@yandex.ru>
2020-09-28 18:04:49 +0300

    Added code shortening

--- encryptor.rb
+++ encryptor.rb
@@ -7,55 +7,26 @@ class Encryptor
     characters.zip(rotated_characters).to_h 
   end
 
-  def encrypt_letter(letter, rotation)
-    cipher_for_rotation = cipher(rotation)
-    cipher_for_rotation[letter]
-  end
-
   def encrypt(string, rotation)
     letter = string.split('')
-    res = []
-    letter.collect do |letter|
-    encrypted_letter = encrypt_letter(letter, rotation)
-    res << encrypted_letter
-    end
-    res.join
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation[letter]
+    letter.map { |letter| cipher_for_rotation[letter] }.join
   end
 
   # Расшифровщик
-  def decipher(rotation)
-    characters = (' '..'z').to_a
-    rotated_characters = characters.rotate(rotation)
-    characters.zip(rotated_characters).to_h.invert  
-  end
-
-  def decrypt_letter(letter, rotation)
-    cipher_for_rotation = decipher(rotation)
-    cipher_for_rotation[letter]
-  end
-
   def decrypt(string, rotation)
     letter = string.split('')
-    res = []
-    letter.collect do |letter|
-    encrypted_letter = decrypt_letter(letter, rotation)
-    res << encrypted_letter
-    end
-    res.join
+    cipher_for_rotation = cipher(rotation).invert
+    cipher_for_rotation[letter]
+    letter.map { |letter| cipher_for_rotation[letter] }.join
   end
 
   # Шифровальщик файла 
   def encrypt_file(filename, rotation)
-    
     if File.exist?(filename)
       file = File.open(filename, 'r') { |f| f.read }
-      letter = file.split('')
-      res = []
-      letter.collect do |letter|
-      encrypted_letter = encrypt_letter(letter, rotation)
-      res << encrypted_letter
-      end
-      file_encrypt = res.join
+      file_encrypt = encrypt(file, rotation)
       newfile = 'secret.txt.encrypted'
       filename.gsub(filename, newfile)
       File.open(newfile, 'w') { |f| f.write(file_encrypt) }
@@ -66,13 +37,7 @@ class Encryptor
    # Расшифровщик файла      
     def decrypt_file(filename, rotation)
       file = File.open(filename, 'r') { |f| f.read }
-      letter = file.split('')
-      res = []
-      letter.collect do |letter|
-      encrypted_letter = decrypt_letter(letter, rotation)
-      res << encrypted_letter
-      end
-      file_decrypt = res.join
+      file_decrypt = decrypt(file, rotation )
       newfile = 'secret.txt.decrypted'
       filename.gsub(filename, newfile)
       File.open(newfile, 'w') { |f| f.write(file_decrypt) }
